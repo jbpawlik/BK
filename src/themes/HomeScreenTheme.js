ceb21cb7fad5baf075fea948982e56368b3e96f9
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-09-07 01:20:26 -0700

    style buttons

--- src/themes/HomeScreenTheme.js
+++ src/themes/HomeScreenTheme.js
@@ -1,7 +1,45 @@
-export const HomeScreenTheme = {
-  ButtonGroup: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
+import { createTheme } from "@rneui/themed/dist/config"
+import { Dimensions } from "react-native";
+
+const DeviceWidth = Dimensions.get('window').width;
+const DeviceHeight = Dimensions.get('window').height;
+
+export const HomeScreenTheme = createTheme({
+  ButtonGrid: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    height: DeviceWidth,
+    width: DeviceWidth,
+    position: 'absolute',
+    bottom: 0
+  },
+  Button1: {
+    width: DeviceWidth/2,
+    height: DeviceWidth/2,
+    backgroundColor: 'lightgreen',
+  },
+  Button2: {
+    width: DeviceWidth/2,
+    height: DeviceWidth/2,
+    backgroundColor: 'orange',
+  },
+  Button3: {
+    width: DeviceWidth/2,
+    height: DeviceWidth/2,
+    backgroundColor: 'lightblue',
+  },
+  Button4: {
+    width: DeviceWidth/2,
+    height: DeviceWidth/2,
+    backgroundColor: 'pink',
+  },
+  Text: {
+    textAlign: 'center',
+    color: 'gold',
+    fontWeight: '900',
+    fontSize: DeviceWidth/14,
+    textShadowColor: 'black',
+    textShadowRadius: 30,
   }
-}
+})
