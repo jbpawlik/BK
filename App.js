d660c83ce1866d8ed4b14eef26fba28ec759c2ce
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-09-07 14:08:55 -0700

    hid status bar

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
+
   return (
     <SafeAreaProvider>
+      <StatusBar
+        hidden={true}
+      />
       <HomeScreen/>
     </SafeAreaProvider>
   );
