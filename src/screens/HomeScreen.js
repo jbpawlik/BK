35d6782c1c76bf27d9cd4b9f45efef12d1123c33
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-09-07 01:21:23 -0700

    move HomeScreen styling

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import ButtonGrid from '../components/ButtonGrid';
-import { ThemeProvider } from "styled-components";
 import { HomeScreenTheme }  from "../themes/HomeScreenTheme";
-
-const DeviceWidth = Dimensions.get('window').width;
-const DeviceHeight = Dimensions.get('window').height;
 
 export default function HomeScreen() {
   return (
-    <ThemeProvider theme={HomeScreenTheme}>
-      <ButtonGrid/>
-    </ThemeProvider>
+    <>
+      <ButtonGrid
+      />
+    </>
   );
 }
