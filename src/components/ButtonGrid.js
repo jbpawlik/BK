bd2f86197da84f83cf9104ff3e56a546db26c444
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-09-07 00:33:03 -0700

    add Buttons

--- src/components/ButtonGrid.js
+++ src/components/ButtonGrid.js
@@ -1,20 +1,64 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import { ButtonGroup } from '@rneui/themed'
+import { StyleSheet, Text, View, StatusBar, ImageBackground } from 'react-native';
+import { Button, ButtonGroup, ThemeProvider } from '@rneui/themed'
+import { HomeScreenTheme } from "../themes/HomeScreenTheme";
 
 export default function ButtonGrid() {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [selectedIndexes, setSelectedIndexes] = useState([0, 2, 3]);
   return (
-    <>
-      <ButtonGroup
-        style='ButtonGroup'
+    <ThemeProvider theme={HomeScreenTheme}>
+      <View
+      style={HomeScreenTheme.ButtonGrid}>
+        <Button
+          buttonStyle={HomeScreenTheme.Button1}
+          title="Bouba"
+          type='outline'
+        >
+          <ImageBackground
+            source={require('../assets/images/bouba-goldinner.png')}
+            style={{width: '100%', height: '100%'}}
+          />
+        </Button>
+        <Button
+          buttonStyle={HomeScreenTheme.Button2}
+          title="Bouba and Kiki"
+          type='outline'
+        >
+          <ImageBackground
+            source={require('../assets/images/bouba+kiki-gold.png')}
+            style={{width: '100%', height: '100%'}}
+          />
+        </Button>
+        <Button
+          buttonStyle={HomeScreenTheme.Button3}
+          title="Neither"
+          type='outline'
+          >
+          <ImageBackground
+            source={require('../assets/images/bouba+kiki-gray.png')}
+            style={{width: '100%', height: '100%'}}
+          />
+        </Button>
+        <Button
+          buttonStyle={HomeScreenTheme.Button4}
+          title="Kiki"
+          type='outline'
+          >
+          <ImageBackground
+            source={require('../assets/images/kiki-goldinner.png')}
+            style={{width: '100%', height: '100%'}}
+          />
+        </Button>
+      </View>
+      {/* <ButtonGroup
+        containerStyle={HomeScreenTheme.ButtonGrid}
+        buttonStyle={HomeScreenTheme.ButtonGroup}
         buttons={['Bouba', 'Kiki', 'Bouba and Kiki', 'Neither']}
         onPress={(value) => {
           setSelectedIndex(value);
         }}
-
-      />
-    </>
+      /> */}
+    </ThemeProvider>
   );
 }
