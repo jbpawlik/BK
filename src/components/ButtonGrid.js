179f03a5dd5dc6c37b37479489370176515e4a94
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-09-08 08:59:45 -0700

    update BulletGrid

--- src/components/ButtonGrid.js
+++ src/components/ButtonGrid.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, StatusBar, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { Button, ButtonGroup, ThemeProvider } from '@rneui/themed'
 import { HomeScreenTheme } from "../themes/HomeScreenTheme";
 
