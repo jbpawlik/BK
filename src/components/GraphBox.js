7ebe195c355d2697a760028a256d03335112d6e2
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-10-11 23:25:58 -0700

    rankings working

--- src/components/GraphBox.js
+++ src/components/GraphBox.js
@@ -1,13 +1,37 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { View, ImageBackground } from "react-native"
 import { TabView, Tab, Image, Tile, Text } from '@rneui/themed'
 import { HomeScreenTheme } from "../themes/HomeScreenTheme";
 
-export default function GraphBox({handleSelect, borderColor}) {
+export default function GraphBox({handleSelect, borderColor, rankings, selectedButton, graphToShow, graphBackgroundColor, setGraphBackgroundColor}) {
+
+  useEffect(() => {
+    if (selectedButton == 'bouba') {
+      setGraphBackgroundColor('lightsalmon')
+    }
+    if (selectedButton == 'kiki') {
+      setGraphBackgroundColor('pink')
+    }
+    if (selectedButton == 'both') {
+      setGraphBackgroundColor('orange')
+    }
+    if (selectedButton == 'neither') {
+      setGraphBackgroundColor('lightblue')
+    }
+  }, [selectedButton, graphBackgroundColor]);
 
   return (
-      <View style={[HomeScreenTheme.GraphBox, {borderColor: borderColor}]}>
-        <Text style={HomeScreenTheme.BoxText}>Graph</Text>
+      <View style={[HomeScreenTheme.GraphBox, {borderColor: borderColor, backgroundColor: graphBackgroundColor}]}>
+        { !graphToShow || Object.values(rankings).length <= 0 ?
+        // { selectedButton == undefined  ?
+          <Text style={HomeScreenTheme.BoxText}>RANKINGS</Text>
+          :
+          <>
+            <Text style={HomeScreenTheme.GraphText}>{rankings[graphToShow][0].toUpperCase()}</Text>
+            <Text style={HomeScreenTheme.GraphText}>{rankings[graphToShow][1].toUpperCase()}</Text>
+            <Text style={HomeScreenTheme.GraphText}>{rankings[graphToShow][2].toUpperCase()}</Text>
+          </>
+          }
       </View>
   )
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect } from "react";
 import { StyleSheet, View, Dimensions } from 'react-native';
 import ButtonGrid from '../components/ButtonGrid';
 import { HomeScreenTheme }  from "../themes/HomeScreenTheme";
@@ -6,24 +6,22 @@ import ThingBox from "../components/ThingBox";
 import HomeBox from "../components/HomeBox";
 import GraphBox from "../components/GraphBox";
 import { ThemeProvider, TabView, Tab, Image, Tile, Text } from '@rneui/themed';
-import { getDatabase, ref, set, get, onValue, child } from "firebase/database";
+import { getDatabase, ref, set, get, update, onValue, child } from "firebase/database";
 import { faker } from '@faker-js/faker';
 
 export default function HomeScreen() {
   const [index, setIndex] = useState(1);
   const [selectedButton, setSelectedButton] = useState(undefined);
   const [borderColor, setBorderColor] = useState('gold');
+  const [graphBackgroundColor, setGraphBackgroundColor] = useState('lemonchiffon')
   const [thingsList, setThingsList] = useState([]);
+  const [selectedThing, setSelectedThing] = useState(undefined)
+  const [rankings, setRankings] = useState({})
+  const [graphToShow, setGraphToShow] = useState(undefined)
 
-  function recordVote(id, thing, bouba, both, neither, kiki, votes, rank) {
-    const db = getDatabase();
-    set(ref(db, 'things/'+ id), {thing: thing, bouba: bouba, both: both, neither: neither, kiki: kiki, rank: rank})
-  }
-  
   useEffect(() => {
     const db = getDatabase();
     const dbRef = ref(db, 'things')
-    console.log(dbRef)
     const thingArray = [];
     onValue(dbRef, (snapshot) => {
       snapshot.forEach((thing) => {
@@ -32,7 +30,68 @@ export default function HomeScreen() {
       setThingsList(thingArray)
     })
   }, [])
+
+  useEffect(() => {
+    // console.log(thingsList[0].kiki)
+    let tempRankings = {}
+    let both = []
+    let neither = []
+    let bouba = []
+    let kiki = []
+    if(thingsList && Object.keys(rankings).length <= 0 ) {
+      thingsList.forEach((thing) => {
+        if(thing.both > 0) {
+          both.push([thing.id, thing.thing, thing.both])
+        }
+        if(thing.neither > 0) {
+          neither.push([thing.id, thing.thing, thing.neither])
+        }
+        if(thing.bouba > 0) {
+          bouba.push([thing.id, thing.thing, thing.bouba])
+        }
+        if(thing.kiki > 0) {
+          kiki.push([thing.id, thing.thing, thing.kiki])
+        }
+        })
+        if(both.length > 0 && neither.length > 0 && bouba.length > 0 && kiki.length > 0) {
+        both.sort((a,b) => {
+          return b[2] - a[2]
+        })
+        neither.sort((a,b) => {
+          return b[2] - a[2]
+        })
+        bouba.sort((a,b) => {
+          return b[2] - a[2]
+        })
+        kiki.sort((a,b) => {
+          return b[2] - a[2]
+        })
+        tempRankings = {
+          "both": [`#1: ${both[0][1]}`, `#2: ${both[1][1]}`, `#3: ${both[2][1]}`],
+          "neither": [`#1: ${neither[0][1]}`, `#2: ${neither[1][1]}`, `#3: ${neither[2][1]}`],
+          "bouba": [`#1: ${bouba[0][1]}`, `#2: ${bouba[1][1]}`, `#3: ${bouba[2][1]}`],
+          "kiki": [`#1: ${kiki[0][1]}`, `#2: ${kiki[1][1]}`, `#3: ${kiki[2][1]}`]
+        }
+      }
+      if(Object.values(tempRankings).length > 0) {
+        setRankings(tempRankings)
+      }
+    }
+  }, [rankings, graphToShow]);
+
+  useEffect(() => {
+    if (thingsList) {
+      setSelectedThing(thingsList[Math.floor(Math.random() * 4999)])
+    } else {
+      setSelectedThing('TEST')
+    }
+  }, [thingsList]);
+
   // Database seed
+  // function recordVote(id, thing, bouba, both, neither, kiki, votes, rank) {
+  //   const db = getDatabase();
+  //   set(ref(db, 'things/'+ id), {id: id, thing: thing, bouba: bouba, both: both, neither: neither, kiki: kiki, rank: rank})
+  // }
   // useEffect(() => {
   //   faker.locale='en_US'
   //   const seedData = faker.helpers.uniqueArray(faker.word.noun, 4999)
@@ -48,11 +107,22 @@ export default function HomeScreen() {
   // }, []);
 
   useEffect(() => {
-    console.log(selectedButton)
+    // console.log(selectedButton)
   }, [selectedButton]);
 
   function handleSelect(buttonValue) {
+    const db = getDatabase();
     setSelectedButton(buttonValue)
+    // console.log(selectedThing)
+    if (index == 0) {
+      update(ref(db, 'things/' + selectedThing.id), {
+        [buttonValue]: selectedThing[buttonValue] + 1
+      })
+      setSelectedThing(thingsList[Math.floor(Math.random() * 4999)])
+    }
+    if (index == 2) {
+      setGraphToShow(buttonValue)
+    }
   }
 
   useEffect(() => {
@@ -113,11 +183,13 @@ export default function HomeScreen() {
                 handleSelect={handleSelect}
                 borderColor={borderColor}
                 thingsList={thingsList}
+                selectedThing={selectedThing}
               />
               <ButtonGrid
                 borderColor={borderColor}
                 handleSelect={handleSelect}
                 selectedButton={selectedButton}
+                setSelectedThing={setSelectedThing}
               />
             </>
           </TabView.Item>
@@ -136,7 +208,12 @@ export default function HomeScreen() {
           <TabView.Item style={HomeScreenTheme.GraphScreen}>
             <>
               <GraphBox
-                borderColor={borderColor}              
+                borderColor={borderColor}
+                rankings={rankings}
+                selectedButton={selectedButton}
+                graphToShow={graphToShow}
+                graphBackgroundColor={graphBackgroundColor}
+                setGraphBackgroundColor={setGraphBackgroundColor}
               />
               <ButtonGrid
                 borderColor={borderColor}
